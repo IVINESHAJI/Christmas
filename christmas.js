9e17fde690c12dd3530779b8cafc7264cd823c73
IVINE SHAJI <ivineshaji123@gmail.com>
2023-11-06 09:09:43 +0400

    Added closing pic

--- christmas.js
+++ christmas.js
@@ -1,19 +1,28 @@
 let optclicked = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
     let red = document.querySelector('.redline');
     let optclick = document.querySelector('.dropdown');
+    let close = document.querySelector('.close');
     red.addEventListener('click',() => {
         if (!optclicked) {
             optclicked = true;
             optclick.style.display = "block";
-
-        }
-
-        else {
-            optclicked = false;
-            optclick.style.display = "none";
+            red.style.display = "none";
+            close.style.display = "block";
         }
 
     });
+
+    close.addEventListener("click", () => {
+        if(optclicked) {
+            optclicked = false;
+            optclick.style.display = "none";
+            red.style.display = "block";
+            close.style.display = "none";
+        }
+    });
+
+
 });
