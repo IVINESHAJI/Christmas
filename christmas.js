0f0272f7a3fe8fdb3601ac9b7c2071061ca15060
IVINE SHAJI <ivineshaji123@gmail.com>
2023-11-11 20:16:07 +0400

    Added login

--- christmas.js
+++ christmas.js
@@ -3,11 +3,11 @@ let clicked = false;
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    let red = document.querySelector('.redline');
+    let optclick = document.querySelector('.dropdown');
+    let close = document.querySelector('.close');
     setInterval(() => {
 
-        let red = document.querySelector('.redline');
-        let optclick = document.querySelector('.dropdown');
-        let close = document.querySelector('.close');
 
         red.addEventListener('click',() => {
             if (!optclicked) {
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 optclick.style.right = "0";
                 red.style.display = "none";
                 close.style.display = "block";
+                optclick.addEventListener('mouseleave', () => {
+                    optclick.style.right = "-20vw";
+                    optclicked = false;
+                    red.style.display = "block";
+                    close.style.display = "none";
+                });
             }   
         });
             
@@ -29,6 +35,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }, 1000);   
     
+    let loginout = document.querySelector(".log");
+    let sec = document.querySelector('section');
+
+    loginout.addEventListener('click', () => {
+        sec.style.display = 'flex';
+        optclick.style.right = '-20vw';
+        optclicked = false;
+    });
+
+    let close_1 = document.querySelector('.close_1');
+
+    close_1.addEventListener('click',() =>  {
+        window.location.href = 'christmas.html';
+    });
     
 });
 
